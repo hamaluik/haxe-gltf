0cd1e1b27a1974eafdef68b86ed3b083a453085d
Kenton Hamaluik <kenton@hamaluik.com>
2017-11-04 10:17:40 -0600

    Started adding tests

--- test/gltf/TestParsing.hx
+++ test/gltf/TestParsing.hx
@@ -8,7 +8,17 @@ using gltf.GLTF;
 class TestParsing extends BuddySuite {
 	public function new() {
 		describe('Parsing GLTF', {
-			it("should parse gltf files");
+			var boxSrc:String = haxe.Http.requestUrl('https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Box/glTF/Box.gltf');
+
+			it("should parse gltf files", {
+				var gltf:GLTF = GLTF.parse(boxSrc);
+
+				gltf.raw.asset.generator.should.be("COLLADA2GLTF");
+				gltf.raw.asset.version.should.be("2.0");
+
+				gltf.raw.scene.should.be(0);
+			});
+
 			it("should parse glb files");
 		});
 	}
